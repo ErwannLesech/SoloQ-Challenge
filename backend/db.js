d42bfad73ed8157f663d8a466fd65ffb0df01fd5
R-One <erwann.lesech@epita.fr>
2025-06-28 19:45:00 +0200

    deploy<back>: adjust db connection

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/*const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT
+});*/
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render-hosted PostgreSQL
+  },
 });
 
 module.exports = pool;
