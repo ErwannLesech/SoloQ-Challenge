eff68fff3932b90bafece4d58595963e85c2fc7b
R-One <erwann.lesech@epita.fr>
2025-06-28 19:34:27 +0200

    deploy<front>: accept domain render

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['soloq-challenge-5nx0.onrender.com'],
+  },
 });
